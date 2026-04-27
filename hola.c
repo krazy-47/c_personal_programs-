1150666e373229fe71bb52e92684dd4db70f0222
Saltaformajo <corp@dev.null>
2026-04-27 23:34:30 +0000

    circle and sphere calculator with area, perimeter, and volume

--- hola.c
+++ hola.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
-int main() {
-   int number1, number2, z;
-   printf("simple calculator enter 1 number to sum up: ");
-  scanf("%d", &number1);
-  printf("please enter the second number: ");
-  scanf("%d", &number2);
-  z = number1 + number2;
-  printf("%d\n", z);
+#include <math.h>
 
+int main() {
+   double radius;
+   double area;
+   double perimiter;
+   double Volume;
+   const double PI = 3.1415926535897932384626433832795028841;
+
+
+
+ printf("enter the radius in cm : ");
+ scanf("%lf", &radius);
+ area = PI * pow(radius, 2);
+ perimiter = 2 * PI * radius;
+ Volume = (4.0 / 3.0) * PI * pow(radius, 3);
+ printf("this is the area %.5lf cm2\n", area );
+ printf("the perimeter is %.5lf cm\n", perimiter);
+printf("this is the volume %.5lf cm3\n", Volume);
    return 0;
 }  
